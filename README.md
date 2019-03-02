ade51c261e0bece338194b065ba5d7c55fd3b7eb
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2019-03-02 02:38:51 +0000

    fix(requirejs)

---
+++ README.md
@@ -0,0 +1,3 @@
+# cloakroom-smart-buffer-proxy-integration-test
+
+The test project for `cloakroom-smart-buffer-proxy`, currently for manual testing, but a starting point for something that's automatic.
--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const { Proxy } = require('cloakroom-smart-buffer-proxy');
 
-console.log(Proxy);
-const instance = new Proxy(64);
+const instance = new Proxy(9);
+console.log(instance.getReadOnlyBuffer());
+instance.append(Buffer.from([0x31, 0x33, 0x32, 0x34]));
+console.log(instance.getReadOnlyBuffer());
+instance.append(Buffer.from([0x41, 0x43, 0x42, 0x44]));
+console.log(instance.getReadOnlyBuffer());
+instance.append(Buffer.from([0x54]));
+console.log(instance.getReadOnlyBuffer());
+instance.append(Buffer.from([0x64]));
+console.log(instance.getReadOnlyBuffer());
+
+instance.append(Buffer.from([0x74, 0x75, 0x76]));
+console.log(instance.getReadOnlyBuffer());
